67beadb7fbff6fbe4fc54fa390a50ea993fe4e7f
Will Ruddick <willruddick@gmail.com>
2026-05-13 14:34:07 +0300

    Update Godot configuration and service worker cache version for improved asset management

--- web/index.service.worker.js
+++ web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778671236|14256445';
+const CACHE_VERSION = '1778672022|13186604';
 /** @type {string} */
 const CACHE_PREFIX = 'Social Soil-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
